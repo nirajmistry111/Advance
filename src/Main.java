b54c21fdfbf9e857acc72cf03b4394ee3870d282
Niraj Mistry <niraj.mistry@dcmail.ca>
2023-07-02 10:47:24 -0500

    First 1.0

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+ Welcome to GDB Online.
+ GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+ Code, Compile, Run and Debug online from anywhere in world.
+
+ *******************************************************************************/
+
+class library{
+    public String addBook(){
+
+    }
+
+    public String issueBook(){
+
+    }
+
+    public String returnBook(){
+
+    }
+
+    public String availableBook(){
+        String [] available = ["wonderwoman", "cars", "mia"];
+        return available ;
+    }
+
+
+}
+public class Main
+{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
